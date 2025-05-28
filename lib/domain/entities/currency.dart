6540497c36904594054566421e4a13ad4b232376
Carmine Maggio <c.maggio@tiendasdaka.com>
2025-05-28 09:45:36 -0400

    feat: estructure and currency entities

---
+++ lib/domain/entities/currency.dart
@@ -0,0 +1,29 @@
+class Currency {
+  final String name;
+  final String pluralName;
+  final String shortName;
+  final SymbolPosition symbol;
+  final String symbolPosition;
+  final String isoCode;
+  final String isoNumeric;
+  final int decimalDigits;
+  final String decimalSeparator;
+  final String thousandsSeparator;
+  final String flag;
+
+  Currency({
+    required this.name,
+    required this.pluralName,
+    required this.shortName,
+    required this.symbol,
+    required this.symbolPosition,
+    required this.isoCode,
+    required this.isoNumeric,
+    required this.decimalDigits,
+    required this.decimalSeparator,
+    required this.thousandsSeparator,
+    required this.flag,
+  });
+}
+
+enum SymbolPosition { before, after, beforeWithSpace, afterWithSpace }
